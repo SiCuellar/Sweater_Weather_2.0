080f902869513ed4179cdf40064dadf29ed8e571
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-02-25 01:13:16 -0700

    added flickr service

--- spec/services/flickr_service_spec.rb
+++ spec/services/flickr_service_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 describe 'flickr servive' do
-  xit "returns image based on location" do
-    # city_backround_image = FlickrService.new.get_city_backround("denver, co")
+  it "returns image based on location" do
+    city_backround_image = FlickrService.new.get_city_backround("denver, co")
 
-    # expect(city_backround_image).to be_an_instance_of(FlickerService)
+    expect(city_backround_image).to have_key(:id)
+    expect(city_backround_image).to have_key(:owner)
+    expect(city_backround_image).to have_key(:secret)
+    expect(city_backround_image).to have_key(:server)
+    expect(city_backround_image).to have_key(:farm)
+    expect(city_backround_image).to have_key(:title)
+    expect(city_backround_image).to have_key(:ispublic)
+    expect(city_backround_image).to have_key(:isfriend)
+    expect(city_backround_image).to have_key(:isfamily)
   end
 end
