f47c28b9d8117b867503cb4528ce7615555ab71e
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-02-27 12:31:55 -0700

    please work

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,21 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-    new_user = User.create_user(user_params)
-    response = {"api_key" => "#{new_user.api_key}"}
+
+    # new_user = User.create_user(user_params)
+    user = User.create(create_user_params)
+    user[:api_key] = SecureRandom.uuid
+    response = {"api_key" => "#{user.api_key}"}
     render json: response
   end
 
   private
 
   def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+
+  def create_user_params
     params.permit(:email, :password, :password_confirmation)
   end
 end
